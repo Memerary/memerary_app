dfc5b6f0279a3e843ebd43b23224a1a357d918c1
nolinuxnopartyy <54733232+nolinuxnopartyy@users.noreply.github.com>
2020-07-20 22:16:08 +0530

    Home Page Static Page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:memerary/main.dart';
+import 'package:Memerary/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
